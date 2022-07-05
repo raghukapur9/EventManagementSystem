0b5382b0a355b11a0a29cbc508e617dcf4191edb
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-07-05 18:43:53 +0530

    updated migrations file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,28 @@
-const Migrations = artifacts.require("Migrations");
+const nftContract = artifacts.require("TicketNFT")
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+
+const eventManagement = artifacts.require("EventManagement");
+
+
+const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+
+module.exports = async function (deployer) {
+    let nftContract;
+    try {
+      nftContract = await nftContract.deployed()    
+    } catch (error) {
+        if (error.message == "nftContract has not been deployed to detected network (network/artifact mismatch)") {
+            await console.log("Deploying nftContract Contract");
+            nftContract = await deployer.deploy(nftContract);
+            await console.log("nftContract Address " + nftContract.address);
+        } else {
+            console.error(error)
+        };
+    }
+    
+    // Factory
+    let eventManagement = await deployProxy(factory, [eventManagement.address], { kind: 'uups' });
+    await console.log("eventManagement Address " + eventManagement.address);
+
+    await nftContract.setController(eventManagement.address);
 };
